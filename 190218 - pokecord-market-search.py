c849645adbc209f6af9e580bf0e9c7ce782dc8fd
anthony62490 <anthonyjmagno@gmail.com>
2019-02-18 09:24:01 -0600

    fixed minor bug. numbers don't go below zero now

--- 190218 - pokecord-market-search.py
+++ 190218 - pokecord-market-search.py
@@ -7,17 +7,17 @@ def printCommand():
     if(name == "EXIT"):
         return False
     hp = int(input("HP: "))
-    hp -= 1
+    hp -= 1 if hp >0 else 0
     atk = int(input("ATK: "))
-    atk -= 1
+    atk -= 1 if atk >0 else 0
     defense = int(input("DEF: "))
-    defense -= 1
+    defense -= 1 if defense >0 else 0
     spatk = int(input("SPATK: "))
-    spatk -= 1
+    spatk -= 1 if spatk >0 else 0
     spdef = int(input("SPDEF: "))
-    spdef -= 1
+    spdef -= 1 if spdef >0 else 0
     speed = int(input("SPEED: "))
-    speed -= 1
+    speed -= 1 if speed >0 else 0
     while True:
         print("p!market search --name %s --hpiv > %s --atkiv > %s --defiv > %s --spatkiv > %s --spdefiv > %s --speediv > %s --order price a --showiv" % (name, hp, atk, defense, spatk, spdef, speed))
         decrement = input("Decrement numbers? (y/n) ")
