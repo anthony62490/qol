0e747b3ce66b667d5f739c75924fa9f1e8d13f64
anthony62490 <anthonyjmagno@gmail.com>
2019-02-19 03:05:21 -0600

    improved script to remove busy work

--- 190218 - pokecord-market-search.py
+++ 190218 - pokecord-market-search.py
@@ -3,20 +3,31 @@
 ###############################################################################
 
 def printCommand():
-    name = input("input species name or EXIT to quit: ")
-    if(name == "EXIT"):
+    print("input blob or EXIT to quit: ")
+    blob = []
+    while True:
+        try:
+            line = input("")
+            if not line:
+                raise EOFError
+        except EOFError:
+            break
+        blob.append(line)
+    if(blob[0] == "EXIT"):
         return False
-    hp = int(input("HP: "))
+
+    name = blob[0].split(' ')[-1]
+    hp = int(blob[4].split(' ')[-1].split('/')[0])
     hp -= 1 if hp >0 else 0
-    atk = int(input("ATK: "))
+    atk = int(blob[5].split(' ')[-1].split('/')[0])
     atk -= 1 if atk >0 else 0
-    defense = int(input("DEF: "))
+    defense = int(blob[6].split(' ')[-1].split('/')[0])
     defense -= 1 if defense >0 else 0
-    spatk = int(input("SPATK: "))
+    spatk = int(blob[7].split(' ')[-1].split('/')[0])
     spatk -= 1 if spatk >0 else 0
-    spdef = int(input("SPDEF: "))
+    spdef = int(blob[8].split(' ')[-1].split('/')[0])
     spdef -= 1 if spdef >0 else 0
-    speed = int(input("SPEED: "))
+    speed = int(blob[9].split(' ')[-1].split('/')[0])
     speed -= 1 if speed >0 else 0
     while True:
         print("p!market search --name %s --hpiv > %s --atkiv > %s --defiv > %s --spatkiv > %s --spdefiv > %s --speediv > %s --order price a --showiv" % (name, hp, atk, defense, spatk, spdef, speed))
